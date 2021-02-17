f1773a1c2f2cbb327f783b35359973f2a4925dc7
Bodea Tudor <bodea.tudor99@yahoo.com>
2021-02-17 20:09:08 +0200

    *added functionality for updating the phone number by clicking the pencil icon

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 window.PhoneBook = {
     API_BASE_URL: "http://localhost:8084/phone-book",
 
+    editId: -1,
+
     createItem: function () {
         var firstName = $("#first-name-field").val();
         var lastName = $("#last-name-field").val();
@@ -46,15 +48,41 @@ window.PhoneBook = {
                 <td>${item.lastName}</td>
                 <td>${item.phoneNumber}</td>
                 <td><a href="#" class="delete-person-from-agenda fa fa-trash"></a>
-                    <a href="#" class="edit-number-from-agenda fa fa-pencil"></a> </td>
+                    <a href="#" class="edit-number-from-agenda fa fa-pencil" data-id="${item.id}" data-phone-number="${item.phoneNumber}"></a> </td>
             </tr>`
+    },
+
+    updateItem: function (itemId, phoneNumber) {
+        $.ajax({
+            url: PhoneBook.API_BASE_URL + "?id=" + itemId,
+            method: "PUT",
+            contentType: "application/json",
+            data: JSON.stringify({
+                phoneNumber: phoneNumber
+            })
+        }).done(function (response) {
+            PhoneBook.getItems();
+            location.reload();
+        })
     },
 
     bindEvents: function () {
         $("#new-person-form").submit(function (event) {
             event.preventDefault();
 
-            PhoneBook.createItem();
+            if (PhoneBook.editId < 0) {
+                PhoneBook.createItem();
+            } else {
+                var phoneNumber = $("#phone-number-field").val();
+                PhoneBook.updateItem(PhoneBook.editId, phoneNumber);
+            }
+        });
+
+        $("#agenda-items-table").delegate(".edit-number-from-agenda", "click", function (event) {
+            event.preventDefault();
+
+            var itemId = $(this).data("id");
+            PhoneBook.editId = itemId;
         });
     }
 };
